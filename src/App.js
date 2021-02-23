f117952814a34f387175bf84e31986cb23c84089
Lincs Navarro <65964131+lincolnnavarro@users.noreply.github.com>
2021-02-23 12:10:32 -0300

    CSS 50%
    Muito CSS feito pqp to cansado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  return(
+    <body>
+      <header id = 'headerhigh' >
+         <div id = 'logo'>
+         <img src="/components/images/PMSPT.png" alt="Logo Prefeitura" />
+        </div><p id = 'h1titulo' >
+          DSV Digital
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>
+          Versão 1.0
+        </span>
       </header>
-    </div>
-  );
+        <nav></nav>
+        <div id = "corpo">
+          <p id = "texto1">
+          O DSV DIGITAL é uma ferramenta desenvolvida pela Secretaria Municipal de Mobilidade e Transportes de São Paulo - 
+          SMT, através do Departamento de Operação do Sistema Viário - DSV, para viabilizar o atendimento mais ágil aos 
+          proprietários de veículos. Através deste canal, o usuário poderá iniciar processos de: Indicação de Condutor, 
+          Defesa da Autuação, 2ª via de Notificações de Autuação e Auto de infração de Trânsito - AIT.
+          </p>
+          <p id = "texto2">
+          Se você já é cadastrado, basta usar seu e-mail e senha.<br/>Se este for seu primeiro acesso, será necessário ter em mãos seu CPF ou CNPJ e RENAVAM.
+          </p>
+          <div id = "caixa1">
+            <div id = "login">
+              <div>
+              <h2>Já sou cadastrado</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+    </body>
+  )
 }
 
 export default App;
